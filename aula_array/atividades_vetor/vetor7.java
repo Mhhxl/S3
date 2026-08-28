37387a27e6436ed8dc6cf7b4e3a7dfe274fc7c43
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-08-28 10:40:04 -0300

    'salvando'

--- aula_array/atividades_vetor/vetor7.java
+++ aula_array/atividades_vetor/vetor7.java
@@ -1,4 +1,7 @@
 package atividades_vetor;
+import java.time.*;
+import java.util.Scanner;
+import java.util.ArrayList;
 
 // Uma loja precisa de um sistema simples para controlar seus produtos.
 // Você deverá criar um programa em Java que permita:
@@ -27,10 +30,74 @@ package atividades_vetor;
 public class vetor7 {
     public static void main(String[] args) {
         
+        Scanner sc = new Scanner(System.in);
+        ArrayList<String> produtos = new ArrayList<>();
         
+        int quantidade_produtos = 0;
+        
+        while (true) {
+            
+            System.out.println("==MENU==");
+            System.out.println("1- Cadastrar produto");
+            System.out.println("2 - Listar produtos");
+            System.out.println("3 - Pesquisar produto");
+            System.out.println("4 - Alterar produto");
+            System.out.println("5 - Sair");
 
+        int escolha_menu = sc.nextInt();
+        sc.nextLine();
+        
+        switch (escolha_menu) {
+            case 1:
+                System.out.println("Qual o produto deseja cadastrar?");
+                String cadastrar_produto = sc.nextLine();
+                produtos.add(cadastrar_produto);
+                System.out.println("O produto " +cadastrar_produto+ " foi cadastrado com sucesso");
+                quantidade_produtos += 1;
+                break;
+            case 2:
+                 for (int i = 0; i < produtos.size(); i++){
+                    System.out.println(produtos.get(i));
+                    }
+                    break;
+            case 4:
+                    for(int i =0; i < produtos.size(); i ++){
+                        System.out.println(i + " = " + produtos.get(i));
 
-
+                    }
+                    
+                    System.out.println("Digite o indice do produto -->> ");
+                    int atualizar_indice = sc.nextInt();
+                    sc.nextLine();
+                    
+                    System.out.println("Digite o novo produto -->> ");
+                    String novoproduto = sc.nextLine();
+                    
+                    produtos.set(atualizar_indice, novoproduto); // o .set servep pra subistituir um indice dentro da lista
+                    break;
+                    
+                    case 3:
+                        for(int i =0; i < produtos.size(); i ++){
+                            System.out.println(i + " = " + produtos.get(i));
+                            System.out.println("Digite o indice do produto que deseja pesquisar -->> ");
+                            int remover_produto = sc.nextInt();
+                            sc.nextLine();
+                            produtos.remove(remover_produto);// no .remove coloca apenas de um argumento, o indice q quer apagar
+                        }
+                        break;
+                    case 5:
+                        System.out.println("Saindo...");
+                        TimeUnit.SECONDS.sleep(5);
+                        break;
+                            
+                        }
+                        
+                    }
+                    
+                    
+                sc.close();
+            }
+            
 
     }
 }
